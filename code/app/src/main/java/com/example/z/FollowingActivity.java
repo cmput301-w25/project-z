b052b1d89835ba9931340ceb7d3b33bd70d20676
Sheldon <sheldonece301@gmail.com>
2025-03-01 17:39:54 -0700

    edited some classes to get rid of red errors

--- code/app/src/main/java/com/example/z/FollowingActivity.java
+++ code/app/src/main/java/com/example/z/FollowingActivity.java
@@ -13,13 +13,12 @@ public class FollowingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_notification);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.notification_page), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
 
-    private int id;
 }
--- code/app/src/main/java/com/example/z/Image.java
+++ code/app/src/main/java/com/example/z/Image.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.z;
 
 import android.widget.ImageView;
 
